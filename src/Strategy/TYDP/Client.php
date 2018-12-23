d120bda156698cc7b57b87f6ba5abc19863ce2e8
kyamashiro <yamashiro@rrr-z.co.jp>
2018-12-23 13:37:17 +0900

    『独習デザインパターン』のStrategyパターン

---
+++ src/Strategy/TYDP/Client.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ky
+ * Date: 2018/12/23
+ * Time: 13:26
+ */
+
+namespace Strategy\TYDP;
+
+class Client
+{
+    public function anOperation()
+    {
+        $kyotoPerson = new DialectSpeaker(DialectSpeaker::KYOTO);
+        $kyotoPerson->sayWelcome();
+        $kyotoPerson->sayThanks();
+    }
+}
---
+++ src/Strategy/TYDP/DialectSpeaker.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ky
+ * Date: 2018/12/23
+ * Time: 13:28
+ */
+
+namespace Strategy\TYDP;
+
+class DialectSpeaker
+{
+    const KYOTO = 1;
+    const NAGOYA = 2;
+
+    /**
+     * @var int
+     */
+    private $dialect;
+
+    /**
+     * DialectSpeaker constructor.
+     * @param $dialect
+     */
+    public function __construct(int $dialect)
+    {
+        $this->dialect = $dialect;
+    }
+
+    public function sayWelcome()
+    {
+        switch ($this->dialect) {
+            case self::KYOTO :
+                echo "おいでやす";
+                break;
+            case self::NAGOYA :
+                echo "いりゃあせ";
+                break;
+        }
+    }
+
+    public function sayThanks()
+    {
+        switch ($this->dialect) {
+            case self::KYOTO :
+                echo "おおきに";
+                break;
+            case self::NAGOYA :
+                echo "ありがとう";
+                break;
+        }
+    }
+}
